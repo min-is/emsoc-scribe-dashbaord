96052818bb0958a5eff0be93a5f6760ca274e9ba
Isaac Min <101093502+min-is@users.noreply.github.com>
2025-05-10 08:05:15 -0700

    updated drag function

--- js/utils.js
+++ js/utils.js
@@ -1,21 +1,45 @@
 function makeDraggable(element) {
     let pos1 = 0, pos2 = 0, pos3 = 0, pos4 = 0;
-    element.onmousedown = dragMouseDown;
+
+    let dragHandle = element.querySelector('.panel-header');
+    if (!dragHandle) {
+        dragHandle = element;
+    }
+
+    dragHandle.onmousedown = dragMouseDown;
+
     function dragMouseDown(e) {
-        e = e || window.event; e.preventDefault();
-        pos3 = e.clientX; pos4 = e.clientY;
+        e = e || window.event;
+
+        const targetTagName = e.target.tagName.toUpperCase();
+        if (targetTagName === 'INPUT' || targetTagName === 'TEXTAREA' || targetTagName === 'BUTTON' || targetTagName === 'SELECT') {
+            return;
+        }
+        
+        e.preventDefault();
+
+        pos3 = e.clientX;
+        pos4 = e.clientY;
+
         document.onmouseup = closeDragElement;
         document.onmousemove = elementDrag;
     }
+
     function elementDrag(e) {
-        e = e || window.event; e.preventDefault();
-        pos1 = pos3 - e.clientX; pos2 = pos4 - e.clientY;
-        pos3 = e.clientX; pos4 = e.clientY;
+        e = e || window.event;
+        e.preventDefault();
+        pos1 = pos3 - e.clientX;
+        pos2 = pos4 - e.clientY;
+        pos3 = e.clientX;
+        pos4 = e.clientY;
+
         element.style.top = (element.offsetTop - pos2) + "px";
         element.style.left = (element.offsetLeft - pos1) + "px";
     }
+
     function closeDragElement() {
-        document.onmouseup = null; document.onmousemove = null;
+        document.onmouseup = null;
+        document.onmousemove = null;
     }
 }
 
